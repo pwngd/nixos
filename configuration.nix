57f73e8947232fe6f8628d7399c4f028439d3b9d
pwngd <126140186+pwngd@users.noreply.github.com>
2025-08-07 09:31:34 -0500

    sdfsdfsdfsdsd

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,21 @@
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.image = ./wp.jpg;
 
+  stylix.fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+    };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
